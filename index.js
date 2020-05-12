34a0c7d7ad1bc22e8071e8bd66cce203d512189d
Jessica Germick <jessicagermick@gmail.com>
2020-05-12 22:11:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 ```js
-console.log("First test run of JavaScript in a Lab")
-```
-```js
-console.error("Do not forget the quotation marks!")
-```
-```js
-console.warn("Make sure to check spelling")
+console.error("First test run of JavaScript in a Lab")
 ```
